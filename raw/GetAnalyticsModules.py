db4814c4b355892ddbbf29b7e99c374f428d9769
kaburagisec <satpamsiberindo@gmail.com>
2025-10-14 16:44:18 +0700

    docs(raw): Bump more raw examples

---
+++ raw/GetAnalyticsModules.py
@@ -0,0 +1,34 @@
+[{
+    'Parameters': {
+        'SimpleItem': [
+            {
+                'Name': 'Sensitivity',
+                'Value': '60'
+            }
+        ],
+        'ElementItem': [
+            {
+                '_value_1': [
+                    <Element {http://www.onvif.org/ver10/schema}CellLayout at 0x2201ba8b840>
+                ],
+                'Name': 'Layout',
+                'CellLayout': {
+                    'Transformation': {
+                        'Translate': {
+                            'x': -1.0,
+                            'y': -1.0
+                        },
+                        'Scale': {
+                            'x': 0.090909,
+                            'y': 0.111111
+                        }
+                    }
+                }
+            }
+        ],
+        'Extension': None,
+        '_attr_1': None
+    },
+    'Name': 'MyCellMotionModule',
+    'Type': 'tt:CellMotionEngine'
+}]
