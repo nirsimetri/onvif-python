61b0031961a274d5abecbd50dc3a3adbba33765b
Kaburagi <kaburagisec@proton.me>
2025-10-29 09:06:42 +0700

    refactor: Organize assets project and add ONVIF DB

---
+++ assets/raw/GetScopes.py
@@ -0,0 +1,25 @@
+[{
+    'ScopeDef': 'Fixed',
+    'ScopeItem': 'onvif://www.onvif.org/type/video_encoder'
+}, {
+    'ScopeDef': 'Fixed',
+    'ScopeItem': 'onvif://www.onvif.org/Profile/Streaming'
+}, {
+    'ScopeDef': 'Fixed',
+    'ScopeItem': 'onvif://www.onvif.org/Profile/G'
+}, {
+    'ScopeDef': 'Fixed',
+    'ScopeItem': 'onvif://www.onvif.org/type/audio_encoder'
+}, {
+    'ScopeDef': 'Fixed',
+    'ScopeItem': 'onvif://www.onvif.org/type/ptz'
+}, {
+    'ScopeDef': 'Fixed',
+    'ScopeItem': 'onvif://www.onvif.org/MAC/0c:a6:4c:ea:0c:6f'
+}, {
+    'ScopeDef': 'Fixed',
+    'ScopeItem': 'onvif://www.onvif.org/hardware/CS-H8c-R100-1K2WKFL'
+}, {
+    'ScopeDef': 'Configurable',
+    'ScopeItem': 'onvif://www.onvif.org/name/CS-H8c-R100-1K2WKFL'
+}]
--- raw/GetDeviceInformation.py
+++
@@ -1,7 +0,0 @@
-{
-    "Manufacturer": "EZVIZ",
-    "Model": "CS-H8c-R100-1K2WKFL",
-    "FirmwareVersion": "V5.3.8 build 250612",
-    "SerialNumber": "CS-H8c-R100-1K2WKFL0120250611CCRRBF9716291",
-    "HardwareId": "88",
-}
