91b6bbff7176d2a12c53801347c22f75e030e08b
Kaburagi <135169292+kaburagisec@users.noreply.github.com>
2026-09-11 20:56:45 +0700

    docs: Add missing line breaks in service documentation comments

--- onvif/services/deviceio.py
+++ onvif/services/deviceio.py
@@ -247,6 +247,7 @@ class DeviceIO(ONVIFService):
         """This operation sets the settings of a relay output.
 
         The relay can work in two relay modes:
+
         - Bistable - After setting the state, the relay remains in this state.
         - Monostable - After setting the state, the relay returns to its idle state after the specified time.
         The physical idle state of a relay output can be configured by setting the IdleState to 'open' or 'closed'
--- onvif/services/search.py
+++ onvif/services/search.py
@@ -104,6 +104,7 @@ class Search(ONVIFService):
         FindEvents method.
 
         GetRecordingSearchResults shall block until:
+
         - MaxResults results are available for the response if MaxResults is specified.
         - MinResults results are available for the response if MinResults is specified.
         - WaitTime has expired.
@@ -137,6 +138,7 @@ class Search(ONVIFService):
         specifying the search token returned from this request.
 
         The device shall continue searching until one of the following occurs:
+
         - The entire time range from StartPoint to EndPoint has been searched through.
         - The total number of matches has been found, defined by the MaxMatches parameter.
         - The session has been ended by a client EndSession request.
@@ -169,6 +171,7 @@ class Search(ONVIFService):
         more than MaxResults results.
 
         GetEventSearchResults shall block until:
+
         - MaxResults results are available for the response if MaxResults is specified.
         - MinResults results are available for the response if MinResults is specified.
         - WaitTime has expired.
@@ -201,6 +204,7 @@ class Search(ONVIFService):
         request, specifying the search token returned from this request.
 
         The device shall continue searching until one of the following occurs:
+
         - The entire time range from StartPoint to EndPoint has been searched through.
         - The total number of matches has been found, defined by the MaxMatches parameter.
         - The session has been ended by a client EndSession request.
@@ -231,6 +235,7 @@ class Search(ONVIFService):
         more than MaxResults results.
 
         GetPTZPositionSearchResults shall block until:
+
         - MaxResults results are available for the response if MaxResults is specified.
         - MinResults results are available for the response if MinResults is specified.
         - WaitTime has expired.
@@ -284,6 +289,7 @@ class Search(ONVIFService):
         specifying the search token returned from this request.
 
         The device shall continue searching until one of the following occurs:
+
         - The entire time range from StartPoint to EndPoint has been searched through.
         - The total number of matches has been found, defined by the MaxMatches parameter.
         - The session has been ended by a client EndSession request.
@@ -315,6 +321,7 @@ class Search(ONVIFService):
         more than MaxResults results.
 
         GetMetadataSearchResults shall block until:
+
         - MaxResults results are available for the response if MaxResults is specified.
         - MinResults results are available for the response if MinResults is specified.
         - WaitTime has expired.
