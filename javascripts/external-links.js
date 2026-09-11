1c3b086bcb913f81b22d67c1109dbb4035809938
Kaburagi <135169292+kaburagisec@users.noreply.github.com>
2026-09-11 07:24:45 +0700

    Deployed 811a526 with MkDocs version: 1.6.1

---
+++ javascripts/external-links.js
@@ -0,0 +1,8 @@
+document$.subscribe(() => {
+  document.querySelectorAll('a[href^="http"]').forEach((link) => {
+    if (link.hostname !== window.location.hostname) {
+      link.target = "_blank";
+      link.rel = "noopener noreferrer";
+    }
+  });
+});
